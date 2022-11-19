d28e3aa8722da9fc813a186425710357fd21409a
J.C. Moyer <jcmoyer32@gmail.com>
2022-11-19 10:01:57 -0500

    Work on map02 and map03

--- assets/maps/terrain.tsx
+++ assets/maps/terrain.tsx
@@ -1,4 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="terrain" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="../textures/terrain.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="trees" type="corner" tile="-1">
+   <wangcolor name="Trees" color="#61ff61" tile="-1" probability="1"/>
+   <wangtile tileid="400" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="401" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="402" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="403" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="404" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="405" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="406" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="407" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="432" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="433" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="434" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="435" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="436" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="437" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="439" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="464" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="465" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="466" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="467" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="468" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="469" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="470" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="471" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="496" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="497" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="498" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="499" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="500" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="501" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="502" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="503" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
