171b2c79639fdb7aec092b4b152481967617f64d
J.C. Moyer <jcmoyer32@gmail.com>
2022-11-25 11:55:07 -0500

    Simplify packaging

--- build.zig
+++ build.zig
@@ -14,7 +14,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("ufeffjam2", "src/main.zig");
+    const exe = b.addExecutable("defense_of_ufeff", "src/main.zig");
     linkSDL2(exe);
     stb.link(exe);
     exe.addPackage(stb.stb_image_pkg);
@@ -24,6 +24,8 @@ pub fn build(b: *std.build.Builder) void {
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
+
+    installAssets(b);
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -53,4 +55,68 @@ fn linkSDL2(exe: *std.build.LibExeObjStep) void {
     exe.linkSystemLibrary("version");
     exe.linkSystemLibrary("oleaut32");
     exe.linkSystemLibrary("ole32");
+}
+
+const maps = [_][]const u8{
+    "assets/maps/level_select.tmj",
+    "assets/maps/map01.tmj",
+    "assets/maps/map02.tmj",
+    "assets/maps/map03.tmj",
+};
+const music = [_][]const u8{
+    "assets/music/Daybreak.ogg",
+    "assets/music/Flight of the Fox.ogg",
+    "assets/music/The Demon's Serenade.ogg",
+    "assets/music/Opposing Tribes.ogg",
+};
+const sounds = [_][]const u8{
+    "assets/sounds/blip.ogg",
+    "assets/sounds/bow.ogg",
+    "assets/sounds/build.ogg",
+    "assets/sounds/burn.ogg",
+    "assets/sounds/click.ogg",
+    "assets/sounds/coindrop.ogg",
+    "assets/sounds/flame.ogg",
+    "assets/sounds/frost.ogg",
+    "assets/sounds/gun.ogg",
+    "assets/sounds/hit.ogg",
+    "assets/sounds/shotgun.ogg",
+    "assets/sounds/slash_hit.ogg",
+    "assets/sounds/slash.ogg",
+    "assets/sounds/spawn.ogg",
+    "assets/sounds/stab.ogg",
+    "assets/sounds/warp.ogg",
+};
+const textures = [_][]const u8{
+    "assets/textures/button_badges.png",
+    "assets/textures/button_base.png",
+    "assets/textures/characters.png",
+    "assets/textures/CommonCase.png",
+    "assets/textures/finger.png",
+    "assets/textures/floating_text.png",
+    "assets/textures/level_select_button.png",
+    "assets/textures/menu_button.png",
+    "assets/textures/special.png",
+    "assets/textures/terrain.png",
+    "assets/textures/ui_panel.png",
+    "assets/textures/water_foam.png",
+    "assets/textures/water.png",
+};
+const tables = [_][]const u8{
+    "assets/tables/CommonCase.json",
+    "assets/tables/floating_text.json",
+    "assets/tables/map01_waves.json",
+    "assets/tables/map02_waves.json",
+    "assets/tables/map03_waves.json",
+};
+const extra = [_][]const u8{
+    "attribution.txt",
+    "readme.txt",
+};
+const all_assets = maps ++ music ++ sounds ++ textures ++ tables ++ extra;
+
+fn installAssets(b: *std.build.Builder) void {
+    for (all_assets) |path| {
+        b.installFile(path, path);
+    }
 }
